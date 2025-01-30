417757964e23beb1df7175bd3e0be6f652aeb448
Santiago Graciano <santiago.gracianod@gmail.com>
2025-01-29 20:19:43 -0500

    working with api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::get('/users', function () {
+        return UserResource::collection(User::all());
+    });
+});
